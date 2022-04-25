15bfd7c35f1fe9091a9ef423d5cb0deacd9f743a
kenhardika <kenzamahardika@gmail.com>
2022-04-25 15:23:02 +0700

    update the script
    Change the read status to be inside and outside prototype

--- script.js
+++ script.js
@@ -5,32 +5,66 @@ let authorInput = document.getElementById('author');
 let pagesInput = document.getElementById('pages');
 let read = document.getElementsByName('read');
 let avail = document.getElementsByName('availability');
+const submitButton = document.getElementById('submit');
+ 
+let myLibrary = [];
 
-
-function book(title, author, pages, read, avail) {
+//Main Object Constructor 
+function book(title, author, pages, avail, read) {
     this.title = title; 
     this.author = author;
-    this.pages = pages; 
-    this.read = read;
+    this.pages = pages;
     this.avail = avail;
+    this.read = read;
  };
 
-// book.prototype.read = function() {
-//     console.log()
-// }
+//Add Read in prototype for every book thats been added
+ addBookToLibrary.prototype.read = function() {
+     this.read = true;
+ }
+ addBookToLibrary.prototype.notRead = function() {
+    this.read = false;
+ }
 
-book.prototype.info = function() {
-    console.log(this.title + ", " + "written by " + this.author + ", " + `has ${this.pages} pages. ` + this.read +". "+ this.avail);
-};
-
-let book1 =  new book('Adventure of Shenzhen', 'Liam Kwok', '232', 'Not read yet', 'Available');
-let myLibrary = [book1];
-
+//Use book obj constructor for Add New Book
 function addBookToLibrary(...args) {
     book.apply(this, args);
-    this.status = "This is a new book";
 }
+
+//use this function to add obj to library
 function addToLib(...args) {
-    let newbook = new addBookToLibrary(...args);
-    myLibrary.push(newbook);
+    myLibrary.push(new addBookToLibrary(...args));
+}
+
+//submit button actions
+submitButton.addEventListener('click', submitNewBook);
+function submitNewBook() {
+    addToLib(titleInput.value, authorInput.value, pagesInput.value, addAvail(), addRead());
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+
+    // if (addRead() === "read") {
+    //     addBookToLibrary.read();
+    // }
+    // else {
+    //     addBookToLibrary.notRead();
+    // }
+
+}
+
+function addAvail() {
+    for (var i = 0, length = avail.length; i < length; i++) {
+        if (avail[i].checked) {
+         return avail[i].value;
+        }
+    }
+}
+
+function addRead() {
+    for (var i = 0, length = read.length; i < length; i++) {
+        if (read[i].checked) {
+          return read[i].value;
+        }
+    }
 }
